f2abdacf5273bebecf4cd9d3bf7cb148cbcf5c8d
martinberlin <martin@luckycloud.de>
2023-01-01 19:28:34 +0100

    Post merge corrections

--- include/goodisplay/touch/gdey027T91T.h
+++ include/goodisplay/touch/gdey027T91T.h
@@ -14,19 +14,20 @@
 #include <epdspi.h>
 #include <gdew_colors.h>
 #include <esp_timer.h>
+#include "FT6X36.h" // Touch interface
 
 // Controller: SSD1680 (Solomon)
-#define GDEY027T91_WIDTH 176
-#define GDEY027T91_HEIGHT 264
-#define GDEY027T91_BUFFER_SIZE (uint32_t(GDEY027T91_WIDTH) * uint32_t(GDEY027T91_HEIGHT) / 8)
+#define GDEY027T91T_WIDTH 176
+#define GDEY027T91T_HEIGHT 264
+#define GDEY027T91T_BUFFER_SIZE (uint32_t(GDEY027T91T_WIDTH) * uint32_t(GDEY027T91T_HEIGHT) / 8)
 // 1 byte of this color in the buffer
-#define GDEY027T91_8PIX_BLACK 0xFF
-#define GDEY027T91_8PIX_WHITE 0x00
+#define GDEY027T91T_8PIX_BLACK 0xFF
+#define GDEY027T91T_8PIX_WHITE 0x00
 
-class Gdey027T91 : public Epd
+class Gdey027T91T : public Epd
 {
   public:
-    Gdey027T91(EpdSpi& IO);
+    Gdey027T91T(EpdSpi& IO, FT6X36& ts);
 
     const uint8_t colors_supported = 1;
     const uint8_t partial_supported = 1;
@@ -43,18 +44,26 @@ class Gdey027T91 : public Epd
     void update();
     // Partial update of rectangle from buffer to screen, does not power off
     void updateWindow(uint16_t x, uint16_t y, uint16_t w, uint16_t h, bool using_rotation = true);
+    // TOUCH related
+    void displayRotation(uint8_t rotation); // Rotates both Epd & Touch
+    void touchLoop();
+    void registerTouchHandler(void(*fn)(TPoint point, TEvent e));
+    void(*_touchHandler)(TPoint point, TEvent e) = nullptr;
 
   private:
     EpdSpi& IO;
-    uint8_t _mono_buffer[GDEY027T91_BUFFER_SIZE];
-    uint8_t _buffer1[GDEY027T91_BUFFER_SIZE];
-    uint8_t _buffer2[GDEY027T91_BUFFER_SIZE];
+    FT6X36& Touch;
+
+    uint8_t _mono_buffer[GDEY027T91T_BUFFER_SIZE];
+    uint8_t _buffer1[GDEY027T91T_BUFFER_SIZE];
+    uint8_t _buffer2[GDEY027T91T_BUFFER_SIZE];
 
     bool color = false;
     bool _initial = true;
     bool _debug_buffer = false;
     bool _mono_mode = false;
-        void _PowerOn();
+    
+    void _PowerOn();
     // Ram data entry mode methods
     void _setRamDataEntryMode(uint8_t em);
     void _SetRamArea(uint8_t Xstart, uint8_t Xend, uint8_t Ystart, uint8_t Ystart1, uint8_t Yend, uint8_t Yend1);
