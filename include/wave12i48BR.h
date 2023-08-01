94be94bb8d69c193cf4aa26237b785ff884955d0
martinberlin <martin@luckycloud.de>
2023-08-01 11:23:11 +0200

    Fix #15

--- include/wave12i48BR.h
+++ include/wave12i48BR.h
@@ -40,7 +40,7 @@ class Wave12I48RB : public Epd
   public:
    
     Wave12I48RB(Epd4Spi& IO);
-    bool colors_supported = 3;
+    uint8_t colors_supported = 3;
     
     void drawPixel(int16_t x, int16_t y, uint16_t color);  // Override GFX own drawPixel method
     
